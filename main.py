f784503f04beef61d2eaf6e6109aa2bf72a0e243
mgr098 <mgr098@uit.no>
2022-07-11 15:17:40 +0200

    🎉 initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import time
+import olympe
+from olympe.messages.ardrone3.Piloting import TakeOff, Landing
+
+DRONE_IP = os.environ.get("DRONE_IP", "10.202.0.1")
+
+def test_connection():
+    drone = olympe.Drone(DRONE_IP)
+    drone.connect()
+    assert drone(TakeOff()).wait().success()
+    time.sleep(10)
+    assert drone(Landing()).wait().success()    
+    drone.disconnect()
+
+if __name__ == "__main__":
+    test_connection()
